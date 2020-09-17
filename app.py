d655f66c298393fe206bd5dc0bf753263f6a7b90
AKP-13 <alexpeirson@hotmail.com>
2020-09-17 15:57:45 +0100

    Add ability to type whether you want dragon or penguin name and generate relevant results

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from dragon_name_generator import dragon_name
 from penguin_name_generator import penguin_name
 
-first_name = input("What is your first name?\n")
+choice = input("Do you want to know your Dragon name or Penguin name? Please enter 'Dragon' or 'Penguin'.\n")
 
-birth_month = input("What month were you born? Enter a number from 1 - 12\n")
-
-gender = input("What is your gender? Please enter M or F.\n").capitalize()
-
-outcome = penguin_name(first_name, birth_month, gender)
-
-print(outcome)
+if choice == "Dragon":
+    first_name = input("What is your first name?\n")
+    birth_month = input("What month were you born? Enter a number from 1 - 12\n")
+    print(dragon_name(first_name, birth_month))
+else:
+    first_name = input("What is your first name?\n")
+    birth_month = input("What month were you born? Enter a number from 1 - 12\n")
+    gender = input("What is your gender? Please enter M or F.\n").capitalize()
+    print(penguin_name(first_name, birth_month, gender))
