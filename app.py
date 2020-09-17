50da015b283b1ee9cda1ae8b1506c920d982cac0
AKP-13 <alexpeirson@hotmail.com>
2020-09-17 17:43:43 +0100

    Handle user entering lowercase name

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ def play():
 def generate_name(choice):
     try:
         if choice == "Dragon":
-            first_name = input("What is your first name?\n")
+            first_name = input("What is your first name?\n").capitalize()
             birth_month = input("What month were you born? Enter a number from 1 - 12\n")
             print(dragon_name(first_name, birth_month))
         else:
-            first_name = input("What is your first name?\n")
+            first_name = input("What is your first name?\n").capitalize()
             birth_month = input("What month were you born? Enter a number from 1 - 12\n")
             gender = input("What is your gender? Please enter M or F.\n").capitalize()
             print(penguin_name(first_name, birth_month, gender))
